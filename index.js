50eeddb48a756b75a19c405876fd67c556abbe7f
Rishabh <rishabhbhagat2801@gmail.com>
2024-06-24 13:34:15 -0400

    Added styles to index.html and edited index.js

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 const express = require('express');
 const app = express();
 
-// Middleware Function
-const myMiddleware = (req,res, next) => {
+// Middleware Function: Logs the request details and moves on to the next middleware or route handler
+const myMiddleware = (req, res, next) => {
     console.log("Time:", Date.now());
     console.log("Request Type:", req.method);
     console.log("Request URL:", req.originalUrl);
-    next();
+    next(); // Call the next middleware or route handler in the stack
 }
 
-const SERVER_PORT = 3000;
+const SERVER_PORT = 3000; // Define the port on which the server will listen
 
-// Global Middleware 
+// Global Middleware: This middleware will be executed for every incoming request
 app.use(myMiddleware);
 
-// Route Middleware
-app.use("/query",(req,res,next) => {
+// Route-specific Middleware: This middleware will be executed for requests to "/query" route
+app.use("/query", (req, res, next) => {
     console.log('Query Middleware');
-    next();
+    next(); // Call the next middleware or route handler for "/query" route
 });
 
+// Serve static files from the "views" directory
 app.use(express.static(__dirname + '/views'));
-app.get('/hello',(req,res) => {
-    res.send('Hello World');
+
+// Route for "/hello": Sends a welcome message
+app.get('/hello', (req, res) => {
+    res.send('Hello, Welcome to my Web Page');
 });
 
-app.get('/about',(req,res) => {
-    res.send('About Page');
+// Route for "/about": Sends a introduction message
+app.get('/about', (req, res) => {
+    res.send('Hi, I am Rishabh Vinay Bhagat. This is my assignment-3 of DevOps.');
 });
 
-// Params Route
-// localhost:3000/contact/RishabhVinay/Bhagat
-app.get('/contact/:fn/:ln',(req,res) => {
-    console.log(req.params);
-    res.send('Contact Page : ' + req.params.fn + ' ' + req.params.ln);
+// Route for "/datetime": Sends the current date and time
+app.get('/datetime', (req, res) => {
+    const currentdate = new Date();
+    const datetostring = currentdate.toLocaleString();
+    res.send(`Today's Date and Time: ${datetostring}`);
 });
 
-// Query Route
-// localhost:3000/query?fn=RishabhVinay&ln=Bhagat
-app.get('/query',(req,res) => {
-    console.log(req.query);
-    res.send('Query Page : ' + req.query.fn + ' ' + req.query.ln);
+// Params Route: Example URL - localhost:3000/contact/RishabhVinay/Bhagat
+// This route extracts the first name (fn) and last name (ln) from the URL parameters and sends a response
+app.get('/contact/:fn/:ln', (req, res) => {
+    console.log(req.params); // Log the route parameters
+    res.send('Contact Page : ' + req.params.fn + ' ' + req.params.ln); // Send a response with the full name
 });
 
-//app.get('/index',(req,res) => {
+// Query Route: Example URL - localhost:3000/query?fn=RishabhVinay&ln=Bhagat
+// This route extracts the first name (fn) and last name (ln) from the query string and sends a response
+app.get('/query', (req, res) => {
+    console.log(req.query); // Log the query parameters
+    res.send('Query Page : ' + req.query.fn + ' ' + req.query.ln); // Send a response with the full name from the query parameters
+});
+
+// if want to serve an HTML file for "/index"
+// app.get('/index',(req,res) => {
 //     res.sendFile(__dirname + '/views/index.html');
 // })
 
+// Start the server and listen on the defined port
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running on port ${SERVER_PORT}`);
 });
